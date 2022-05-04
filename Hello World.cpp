ef69bf24c57d5ae8110761917575631de34855c2
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-03 20:37:43 -0700

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout<<"\n\n QWEERTYUIOP :\n";
+	cout<<"_________________________ ;\n";
+	cout<<"Welcome to \n";
+	cout<<"final result";
+	
+}

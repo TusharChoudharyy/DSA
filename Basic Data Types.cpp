edbb3f70c43b9fc108be8bebb5b9bc0a0c0656b4
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-03 20:39:11 -0700

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    scanf("%d\n%lld\n%c\n%f\nLf\n",&a,&b,&c,&d,&e);
+    printf("\nelld\m_c\nf\n1f\n",a,b,c,d,e);
+  }
